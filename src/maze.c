c4216de1f44f172e597d614ef27f26573db64525
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-01-07 23:49:33 -0800

    fix bug so unpathed regions will still be filled

--- src/maze.c
+++ src/maze.c
@@ -15,6 +15,12 @@ void backtrackMaze(Grid* maze, int x, int y, int region) {
   Direction directions[NUM_DIRECTIONS] = { NORTH, EAST, SOUTH, WEST };
   shuffleDirections(directions, NUM_DIRECTIONS);
 
+  const Position fill = {
+        .region = region,
+        .tile = FLOOR,
+  };
+  maze->data[y][x] = fill;
+
   for (int i = 0; i < NUM_DIRECTIONS; ++i) {
     int dx, dy;
     directionToDelta(directions[i], &dx, &dy);
@@ -23,11 +29,6 @@ void backtrackMaze(Grid* maze, int x, int y, int region) {
 
     if (nx >= 0 && nx < maze->width && ny >= 0 && ny < maze->height &&
       noPassages(maze, nx, ny)) {
-      const Position fill = {
-        .region = region,
-        .tile = FLOOR,
-      };
-      maze->data[y][x] = fill;
       maze->data[y + dy][x + dx] = fill;
       maze->data[ny][nx] = fill;
       backtrackMaze(maze, nx, ny, region);
